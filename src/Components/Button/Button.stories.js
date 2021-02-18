0c7c4af652374db75a663a72ab5a97b7b30afda1
Gabriel Bourget <gaboxa@syntronic.com>
2021-02-18 15:41:13 -0500

    adding args to stories

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -4,6 +4,9 @@ import Button from "./Button";
 const storyInfo = {
   title: "Button",
   component: Button,
+  args: {
+    children: "button",
+  }
 };
 
 export default storyInfo;
@@ -12,3 +15,23 @@ export const Primary = () => <Button variant="primary">Primary</Button>
 export const Secondary = () => <Button variant="secondary">Primary</Button>
 export const Success = () => <Button variant="success">Primary</Button>
 export const Danger = () => <Button variant="danger">Primary</Button>
+
+const Template = (props) => <Button {...props} />
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: "primary",
+  children: "Primary Args"
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: "Long Primary Args"
+}
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+  children: "Secondary Args"
+}
