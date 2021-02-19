1808d022518f9abd5a972c7626edb8b239fe7c25
Gabriel Bourget <gaboxa@syntronic.com>
2021-02-18 19:56:33 -0500

    Chakra Button and ThemeProvider

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -11,7 +11,7 @@ const storyInfo = {
 
 export default storyInfo;
 
-export const Primary = () => <Button variant="primary">Primary</Button>
+export const Primary = () => <Button variant="primary">Primary</Button>;
 export const Secondary = () => <Button variant="secondary">Primary</Button>
 export const Success = () => <Button variant="success">Primary</Button>
 export const Danger = () => <Button variant="danger">Primary</Button>
